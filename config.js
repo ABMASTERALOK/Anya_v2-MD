2a4dc855eff110095ba6d017f0fe9155b455c8eb
ABMASTERALOK <112396479+ABMASTERALOK@users.noreply.github.com>
2024-09-24 22:27:58 +0100

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ module.exports = {
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "mongodb+srv://abrahamjoe947:uTxULv4xIsoTeaPu@abraham.gmoot.mongodb.net/",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://abrahamjoe947:ABASIEMEK@abraham.gmoot.mongodb.net/",
     sessionId: process.env.SESSION_ID || "dHBqdG5_Queen-Anya_ma2E=",
 
     /**
